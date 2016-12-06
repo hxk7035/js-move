2afc8694e3b4cd8856a38bd2b795b49000e72233
hxk7035 <hxk349237660@163.com>
2016-12-06 16:28:00 +0800

    Update move.js
    测试保存

--- move.js
+++ move.js
@@ -11,21 +11,48 @@ function getStyle(obj,attr){
 
 
 //var timer=null;
-function startMove(obj,target,fn){
+function startMove(obj,json,fn){
 	clearInterval(obj.timer);
-    obj.timer=setInterval(function(){
-			var speed=(target-parseInt(getStyle(obj,'width')))/10;
-			speed=speed>0?Math.ceil(speed):Math.floor(speed);
+	obj.timer=setInterval(function(){
+		for(var attr in json)
+		{
+			  //  var flag=true;       //用于控制所有运动是否全部完成的标识
+				var cur;             //该属性当前值
+				if(attr == 'opacity')
+				{
+			    	cur=parseFloat(getStyle(obj,attr)*100);
+				}else{
+					cur=parseInt(getStyle(obj,attr));
+				}
+				
+				
+				var speed=(json[attr]-cur)/10;
+				speed=speed>0?Math.ceil(speed):Math.floor(speed);
 			
-			if(getStyle(obj,'width')==target){
-				clearInterval(obj.timer);
-			}
-			else{
-				obj.style.width=parseInt(getStyle(obj,'width'))+speed+'px';
-			}
+				if(cur==json[attr])
+				{
+					//flag=false;
+					clearInterval(obj.timer);
+				}
+				else
+				{
+					if(attr=='opacity')
+					{
+						obj.style.filter='alpha(opacity='+(cur+speed)+')';
+						obj.style.opacity=Math.round((cur+speed)/100);
+					}
+					else
+					{
+						obj.style[attr]=cur+speed+'px';
+					}
+				}
+		
+		}
+		
 		},30)
-	if(fn){
-		fn();
+		if(fn){
+			fn();
+		}
 	}
 	
-}
+	
