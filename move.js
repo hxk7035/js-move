a7b430efe69c94ace67d96304caa4701430b1fd6
hxk7035 <hxk349237660@163.com>
2016-12-06 15:38:11 +0800

    Update move.js
    简单添加宽度的运动

--- move.js
+++ move.js
@@ -1,7 +1,31 @@
-<script>
-window.onload=function(){
-    
+//获取元素的样式，避免边框之类的样式影响原样式的值
+//用getStyle代替offset
+function getStyle(obj,attr){
+    if(obj.currentStyle){
+        return obj.currentStyle[attr];   //针对IE浏览器
+    }
+    else{
+        return getComputedStyle(obj,false)[attr];  //针对火狐浏览器
+    }
+}
 
-  }
 
-</script>
+//var timer=null;
+function startMove(obj,target,fn){
+	clearInterval(obj.timer);
+    obj.timer=setInterval(function(){
+			var speed=(target-parseInt(getStyle(obj,'width')))/10;
+			speed=speed>0?Math.ceil(speed):Math.floor(speed);
+			
+			if(getStyle(obj,'width')==target){
+				clearInterval(obj.timer);
+			}
+			else{
+				obj.style.width=parseInt(getStyle(obj,'width'))+speed+'px';
+			}
+		},30)
+	if(fn){
+		fn();
+	}
+	
+}
